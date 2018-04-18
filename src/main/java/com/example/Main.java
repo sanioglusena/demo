7036e0d2b304534ffd12966d81672d1c65e332ee
busracanak <busra.canak@comeon.com>
2018-04-18 16:50:01 +0300

    first commit

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -57,13 +57,14 @@ public class Main {
   String db(Map<String, Object> model) {
     try (Connection connection = dataSource.getConnection()) {
       Statement stmt = connection.createStatement();
-      stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-      stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-      ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+      //stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+      //stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+
+      ResultSet rs = stmt.executeQuery("SELECT team_name FROM team");
 
       ArrayList<String> output = new ArrayList<String>();
       while (rs.next()) {
-        output.add("Read from DB: " + rs.getTimestamp("tick"));
+        output.add("Read from DB: " + rs.getString("team_name"));
       }
 
       model.put("records", output);
